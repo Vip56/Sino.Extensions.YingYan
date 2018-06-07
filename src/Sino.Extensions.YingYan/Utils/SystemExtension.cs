76095e0f420e61f84f598fb5664e3582fb4a97ce
姚正发 <976691141@qq.com>
2018-06-07 16:59:25 +0800

    完成终端管理

---
+++ src/Sino.Extensions.YingYan/Utils/SystemExtension.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace System
+{
+    public static class SystemExtension
+    {
+        /// <summary>
+        /// 获取Unix时间戳
+        /// </summary>
+        public static long ToUnixTime(this DateTime dt)
+        {
+            TimeSpan ts = dt - new DateTime(1970, 1, 1, 0, 0, 0, 0);
+            return Convert.ToInt64(ts.TotalSeconds);
+        }
+    }
+}
