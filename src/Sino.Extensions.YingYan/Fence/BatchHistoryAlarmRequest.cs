e9119f71bd05dd2beeae6bb02f1688418ad04f0d
1078808874@qq.com <1078808874@qq.com>
2018-06-07 14:28:45 +0800

    增加地理围栏基础实现

--- src/Sino.Extensions.YingYan/Fence/BatchHistoryAlarmRequest.cs
+++ src/Sino.Extensions.YingYan/Fence/BatchHistoryAlarmRequest.cs
@@ -6,14 +6,29 @@ namespace Sino.Extensions.YingYan.Fence
 {
     public class BatchHistoryAlarmRequest
     {
+        /// <summary>
+        /// 开始时间
+        /// </summary>
         public long StartTime { get; set; }
 
+        /// <summary>
+        /// 结束时间
+        /// </summary>
         public long EndTime { get; set; }
 
+        /// <summary>
+        /// 返回坐标类型
+        /// </summary>
         public CoordType CoordTypeOutput { get; set; }
 
+        /// <summary>
+        /// 分页索引
+        /// </summary>
         public int PageIndex { get; set; }
 
+        /// <summary>
+        /// 分页大小
+        /// </summary>
         public int PageSize { get; set; }
     }
 }
