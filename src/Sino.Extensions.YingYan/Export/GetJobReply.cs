d809cd5807253b1d063c673ae566341e79ee7362
1078808874@qq.com <1078808874@qq.com>
2018-06-08 11:20:02 +0800

    增加批量导出轨迹基础实现

---
+++ src/Sino.Extensions.YingYan/Export/GetJobReply.cs
@@ -0,0 +1,23 @@
+﻿using RestSharp.Deserializers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sino.Extensions.YingYan.Export
+{
+    public class GetJobReply : Reply
+    {
+
+        /// <summary>
+        /// 任务总条数
+        /// </summary>
+        [DeserializeAs(Name = "total")]
+        public int Total { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [DeserializeAs(Name = "jobs")]
+        public List<Jobs> Jobs { get; set; }
+    }
+}
