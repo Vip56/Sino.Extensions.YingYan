1517c6d2eab4359bf8d2f38331764769dd104f29
姚正发 <976691141@qq.com>
2018-06-11 13:15:04 +0800

    增加IOC注入

---
+++ src/Sino.Extensions.YingYan/YingYanService.cs
@@ -0,0 +1,10 @@
+﻿namespace Sino.Extensions.YingYan
+{
+    public class YingYanService : IYingYanService
+    {
+        public YingYanService(string url, string ak, string serviceId)
+        {
+
+        }
+    }
+}
